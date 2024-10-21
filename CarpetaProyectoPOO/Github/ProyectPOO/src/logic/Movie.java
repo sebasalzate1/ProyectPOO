f8cd5339bbe3ebbc21c6ed31176d3e4a12e0626c
sebasalzate1 <sebastian.alzate.arenas123@gmail.com>
2024-10-21 16:33:06 -0500

    1

---
+++ CarpetaProyectoPOO/Github/ProyectPOO/src/logic/Movie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+/**
+ *
+ * @author USER
+ */
+public class Movie {
+    private String movieName; 
+    private int idMovie; 
+    
+    
+}
