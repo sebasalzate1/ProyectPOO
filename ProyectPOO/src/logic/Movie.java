4f0b1b3d275735e3b4cc5108ff53b292376ccd20
sebasalzate1 <sebastian.alzate.arenas123@gmail.com>
2024-10-22 18:01:10 -0500

    3

--- ProyectPOO/src/logic/Movie.java
+++ ProyectPOO/src/logic/Movie.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package logic;
+package Logic;
 
 /**
  *
  * @author USER
  */
 public class Movie {
-    private String movieName; 
-    private int idMovie; 
-    
     
 }
